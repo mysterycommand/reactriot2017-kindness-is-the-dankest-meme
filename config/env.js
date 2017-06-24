485c6def2fe0802e2886cdaf2e20c3fee2aeb7ef
Matt Hayes <matt@mysterycommand.com>
2017-06-24 06:28:12 -0400

    style: just shuffling a bit b/c NODE_ENV was already const'd on line 8

--- config/env.js
+++ config/env.js
@@ -5,7 +5,7 @@ const paths = require('./paths');
 // Make sure that including paths.js after env.js will read .env variables.
 delete require.cache[require.resolve('./paths')];
 
-const NODE_ENV = process.env.NODE_ENV;
+const { NODE_ENV } = process.env;
 if (!NODE_ENV) {
   throw new Error(
     'The NODE_ENV environment variable is required but was not specified.',
@@ -56,9 +56,7 @@ process.env.NODE_PATH = (process.env.NODE_PATH || '')
 const REACT_APP = /^REACT_APP_/i;
 
 function getClientEnvironment(publicUrl) {
-  const API_ROOT = process.env.NODE_ENV === 'production'
-    ? ''
-    : 'http://localhost:3001';
+  const API_ROOT = NODE_ENV !== 'production' ? 'http://localhost:3001' : '';
 
   const raw = Object.keys(process.env)
     .filter(key => REACT_APP.test(key))
@@ -70,7 +68,7 @@ function getClientEnvironment(publicUrl) {
       {
         // Useful for determining whether we’re running in production mode.
         // Most importantly, it switches React into the correct mode.
-        NODE_ENV: process.env.NODE_ENV || 'development',
+        NODE_ENV: NODE_ENV || 'development',
         // Useful for resolving the correct path to static assets in `public`.
         // For example, <img src={process.env.PUBLIC_URL + '/img/logo.png'} />.
         // This should only be used as an escape hatch. Normally you would put
