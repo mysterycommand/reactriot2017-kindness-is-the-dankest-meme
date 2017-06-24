c6d8cec9e3b9dc4b26b51d36ab9090716b629a20
Matt Hayes <matt@mysterycommand.com>
2017-06-24 07:46:24 -0400

    refactor: move the coin and crown to their own components

--- src/components/token/index.jsx
+++ src/components/token/index.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import { Circle, Group, Path } from 'react-konva';
+import { Group } from 'react-konva';
 import { number } from 'prop-types';
+
+import Coin from 'components/coin';
+import Crown from 'components/crown';
 
 import randomRgb from '../../utils/random-rgb';
 
@@ -31,34 +34,27 @@ class Token extends Component {
     const { x, y, radius } = this.props;
     const { fill } = this.state;
 
-    const strokeWidth = radius / 6;
-    const halfStrokeWidth = strokeWidth / 2;
-
     const pathWidth = radius * 1.1;
     const pathHeight = pathWidth * 2 / 3;
 
     return (
       <Group onClick={this.onClick}>
-        <Circle radius={radius} x={x} y={y} fill={fill} />
-        <Circle
-          radius={radius - halfStrokeWidth}
-          x={x}
-          y={y}
-          stroke="rgba(0,0,0,0.15)"
-          strokeWidth={strokeWidth}
+        <Coin
+          {...{
+            x,
+            y,
+            radius,
+            fill,
+          }}
         />
-        <Path
-          data={[
-            `M ${x - pathWidth / 2} ${y - pathHeight / 2}`,
-            `L ${x - pathWidth / 4.5} ${y}`,
-            `L ${x} ${y - pathHeight / 1.5}`,
-            `L ${x + pathWidth / 4.5} ${y}`,
-            `L ${x + pathWidth / 2} ${y - pathHeight / 2}`,
-            `L ${x + pathWidth / 2} ${y + pathHeight / 2}`,
-            `L ${x - pathWidth / 2} ${y + pathHeight / 2}`,
-            'Z',
-          ].join()}
-          fill="rgba(0,0,0,0.15)"
+        <Crown
+          {...{
+            x,
+            y,
+            width: pathWidth,
+            height: pathHeight,
+            fill: 'rgba(0,0,0,0.15)',
+          }}
         />
       </Group>
     );
