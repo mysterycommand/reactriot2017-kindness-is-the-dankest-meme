4a0a2b3eaa440eef67e9e1eb4531cbbb578db2a3
Matt Hayes <matt@mysterycommand.com>
2017-06-24 14:13:09 -0400

    refactor: not sure why, but it seems like the order here matters

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 
 const { NODE_ENV, PORT } = process.env;
+
 const port = PORT || (NODE_ENV === 'development' ? 3001 : 3000);
 
 const app = express();
@@ -21,6 +22,12 @@ if (NODE_ENV === 'development') {
 
 app.use(express.static(path.join(__dirname, '../build')));
 
+app.ws('/dungeon', (ws, req) => {
+  ws.on('message', message => {
+    ws.send(message);
+  });
+});
+
 app.get('/api/test', (req, res) => {
   setTimeout(() => {
     const rand = Math.floor(Math.random() * 10);
@@ -38,12 +45,6 @@ if (!NODE_ENV === 'development') {
 
 app.use((req, res, next) => {
   res.status(404).send('oops :(');
-});
-
-app.ws('/dungeon', (ws, req) => {
-  ws.on('message', message => {
-    ws.send(message);
-  });
 });
 
 app.listen(port);
