6e6e954ebd93b9390b24c656255de48eddfc680d
Matt Hayes <matt@mysterycommand.com>
2017-06-24 13:35:56 -0400

    chore: yarn add express-ws

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
-const express = require('express');
 const path = require('path');
+
+const express = require('express');
 
 const { NODE_ENV, PORT } = process.env;
 const __DEV__ = NODE_ENV === 'development';
