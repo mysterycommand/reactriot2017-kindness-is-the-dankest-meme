f941a02000d2547bdd63688fdf0a400804dbf26e
Matt Hayes <matt@mysterycommand.com>
2017-06-24 13:14:05 -0400

    refactor: add the faces to the viewport

--- src/components/viewport/index.jsx
+++ src/components/viewport/index.jsx
@@ -6,12 +6,19 @@ import Token from 'components/token';
 
 import style from './style.scss';
 
-const Viewport = ({ width, height }) =>
-  <Stage className={style.viewport} width={width} height={height}>
-    <Layer>
-      <Token x={width / 2} y={height / 2} radius={48} />
-    </Layer>
-  </Stage>;
+const Viewport = ({ width, height }) => {
+  const halfWidth = width / 2;
+  const halfHeight = height / 2;
+
+  return (
+    <Stage className={style.viewport} width={width} height={height}>
+      <Layer>
+        <Token x={halfWidth - 60} y={halfHeight} radius={48} face="star" />
+        <Token x={halfWidth + 60} y={halfHeight} radius={48} />
+      </Layer>
+    </Stage>
+  );
+};
 
 Viewport.propTypes = {
   width: number.isRequired,
